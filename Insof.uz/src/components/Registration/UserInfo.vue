2c47de62e720a7498662856dcc57712f78d4cf43
Alimardon9459 <mullajonovalimardon@gmail.com>
2022-10-31 12:42:48 +0500

    change backet and regiter page

---
+++ Insof.uz/src/components/Registration/UserInfo.vue
@@ -0,0 +1,37 @@
+<template >
+    <div>
+        <div class="register">
+            <p class="register__title">Персональные данные</p>
+            <q-input outlined v-model="fname" label="Ismingiz" counter  maxlength='30' class=" mt-10px" />
+            <q-input outlined v-model="tel_number" label="Raqamingiz" counter  maxlength='11' class=" mt-10px" />
+            <q-input outlined v-model="adress" label="Manzilingiz" counter  maxlength='100' class=" mt-10px" />
+            <q-input outlined v-model="comment" label="Izohingiz" counter  maxlength='50' class=" mt-10px" />
+        </div>
+        <div class="register__end row justify-around ">
+            <q-btn class="text-white bg-blue-grey-10 mt-30px" icon="location_on" label=" Lakatsiya olish  " />
+            <q-btn class="text-white bg-blue-grey-10 mt-30px" icon="send" label=" Buyurtma berish  " />
+        </div>
+    </div>
+</template>
+<script>
+import {ref} from 'vue';
+export default {
+    setup() {
+        const fname=ref('')
+        const tel_number=ref('+998')
+        const adress=ref('')
+        const comment=ref('')
+        return{
+            fname,
+            tel_number,
+            adress,
+            comment,
+        }
+    }
+}
+</script>
+<style scoped>
+    .register__title{
+        font-size: 32px;
+    }
+</style>
---
+++ Insof.uz/src/pages/RegistrationPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row mt-30px register">
+        <UserInfo class="user-info" />
+        <BasketInfo class="basket-info" />
+    </div>
+</template>
+<script>
+import BasketInfo from 'src/components/BasketComponets/BasketInfo.vue';
+import UserInfo from 'src/components/Registration/UserInfo.vue';
+
+export default {
+    components: { UserInfo, BasketInfo },
+    data() {
+        return {
+        
+        }
+    },
+}
+</script>
+<style  scoped>
+    .user-info{
+        width: 60%;
+    }
+    .basket-info{
+        width: 40%;
+    }
+    @media screen and (max-width:900px) {
+        .user-info{
+        width: 100%;
+        }
+        .basket-info{
+            width: 100%;
+            margin: 50px 0px 50px 0px;
+        }
+    }
+    
+</style>
