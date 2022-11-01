0c257af9b295ddfd00cc2ce09c0f0c1e5a545a84
SanjarbekUsmonov <noobpusk@gmail.com>
2022-11-01 18:04:25 +0500

    imported ref for comp modal

--- Insof.uz/src/components/CompModal.vue
+++ Insof.uz/src/components/CompModal.vue
@@ -1,10 +1,22 @@
 <template >
   <div>
-      iouigdftfygjhfug
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section>
+          <div class="text-h6">Medium</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Click/Tap on the backdrop.
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white text-teal">
+          <q-btn flat label="OK" v-close-popup />
+        </q-card-actions>
+      </q-card>
   </div>
 </template>
 <script>
-
+import { ref } from 'vue'
 export default {
     setup(){
       return{
